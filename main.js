b9f771595699dd06ce7a4fb81d3901086a96b10a
JuanBG20 <juanbgalvan.19@gmail.com>
2023-05-07 21:54:23 -0300

    Buscador Añadido

--- main.js
+++ main.js
@@ -3,13 +3,36 @@ const excelInput = document.getElementById("excelInput");
 const listaDescripcion = document.getElementById("listaDescripcion");
 const listaPrecio = document.getElementById("listaPrecio");
 
+const table = document.getElementById("table");
 const tableHead = document.getElementById("tableHead");
 const tableBody = document.getElementById("tableBody");
 
+const botonBuscador = document.getElementById("botonBuscador");
+const cuadroBusqueda = document.getElementById("inputBuscador");
+
 excelInput.addEventListener("change", async () => {
     const content = await readXlsxFile(excelInput.files[0]);
-
+    
     Excel(content);
+});
+
+botonBuscador.addEventListener("click", () => {
+    const filas = document.querySelectorAll("tbody>tr");
+    let filter = cuadroBusqueda.value.toLowerCase();
+    
+    filas.forEach(fila => {
+        const columna = fila.querySelectorAll("td")[0];
+        const columnaPrecio = fila.querySelectorAll("td")[1];
+        let producto = columna.textContent.toLowerCase();
+
+        if(producto.indexOf(filter) > -1) {
+            fila.style.display = "";
+            columnaPrecio.style.display = "";
+        } else {
+            fila.style.display = "none";
+            columnaPrecio.style.display = "none";
+        }
+    });
 });
 
 function Excel(content) {
@@ -17,11 +40,9 @@ function Excel(content) {
         header: () => {
             content[5].map(header => {
                 if(header != null && header != "Codigo Barra") {
-                    const td = document.createElement("td");
-                
-                    td.innerText = header;
-
-                    tableHead.appendChild(td);
+                    tableHead.innerHTML += `
+                        <td>${header}</td>
+                    `
                 }
             })
         },
@@ -30,8 +51,8 @@ function Excel(content) {
             rows.map(row => {
                 tableBody.innerHTML += `
                     <tr>
-                        <td>${row[1]}</td>
-                        <td>${CalcularPrecio(row[2], 21, 100)}</td>
+                        <td class="articulos">${row[1]}</td>
+                        <td>${CalcularPrecio(row[2], 21, 2)}</td>
                     </tr>
                 `
             })
@@ -45,7 +66,7 @@ function Excel(content) {
 }
 
 function CalcularPrecio(precio, iva, ganancia) {
-    let precioFinal = ((precio * (iva / 100)) + precio) * (ganancia / 100);
+    let precioFinal = ((precio * (iva / 100)) + precio) * ganancia;
 
     return precioFinal;
 }
